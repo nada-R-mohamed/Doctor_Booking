cbc383c2b9500802f63ae428f3da5405301dd1db
Nada <apple@apples-MacBook-Pro.local>
2025-07-17 06:21:47 +0300

    create update database migration and make relations in model

--- app/Models/Appointment.php
+++ app/Models/Appointment.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Appointment extends Model
 {
-    //
+    protected $fillable = [
+        'name',
+        'phone',
+        'date',
+        'time',
+        'complaint',
+    ];
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -45,4 +45,7 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+    public function appointments(){
+        return $this->hasMany(Appointment::class);
+    }
 }
